a437a796d87f6b39cb464204f25196de3a6f8008
Jimin <jimin4124.22@gmail.com>
2024-04-18 00:26:01 +0900

    feat: 공통 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
-import { getEmotionImage } from "./util/get-emotion-image";
+import getEmotionImage from "./util/get-emotion-image";
 
 // 1. “/” : 모든 일기를 조회하는  Home 페이지
 // 2. “/new” : 새로운 일기를 작성하는 New 페이지
@@ -19,19 +21,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header title={"Header"} leftChild={<Button text={"left"} />} rightChild={<Button text={"right"} />} />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useSearchParams } from "react-router-dom";
 
 export default function Home() {
   const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
+  // console.log(params.get("value"));
 
   return <div>Home</div>;
 }
