8d454da531a2163cb754163b335e77dc1bc8fe13
Jimin <jimin4124.22@gmail.com>
2024-04-18 23:24:51 +0900

    feat: edit 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import Edit from "./pages/Edit";
+
 import Button from "./components/Button";
 import Header from "./components/Header";
 
@@ -21,35 +23,11 @@ function App() {
 
   return (
     <>
-      <Header title={"Header"} leftChild={<Button text={"left"} />} rightChild={<Button text={"right"} />} />
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123 버튼 클릭");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
